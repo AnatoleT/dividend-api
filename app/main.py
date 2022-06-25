4af7f90c4c9042969437e8a5acf31e4aba7a2180
AnatoleT <anatole.titeux@gmail.com>
2022-06-25 22:22:10 +0200

    please work

--- app/main.py
+++ app/main.py
@@ -5,12 +5,13 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/dividend")
 def home_view():
+	page = requests.args.get('ticker', default = 1, type = int)
 	session = requests.Session()
 	my_headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", 
           "Accept":"text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,image/apng,*/*;q=0.8"}
-	url = 'https://finviz.com/quote.ashx?t=TTE'
+	url = 'https://finviz.com/quote.ashx?t=' + page
 	response = session.get(url, headers=my_headers)
 	soup = BeautifulSoup(response.text, 'html.parser')
 	tab = soup.find("table",{"class":"snapshot-table2"})

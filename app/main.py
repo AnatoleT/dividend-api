71c6388a3259d3255117e77dbe3ad7d7d1ea76d5
AnatoleT <anatole.titeux@gmail.com>
2022-06-25 21:36:37 +0200

    Nineth Version

--- app/main.py
+++ app/main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
 	data_set = {"Page" : "Home", "Message" : "Hello World"}
-    json_dump = json.dumps(data_set)
-    return json_dump
+	json_dump = json.dumps(data_set)
+	return json_dump

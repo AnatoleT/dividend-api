49913dd69991893932840a5f41039cd7c90f4c64
AnatoleT <anatole.titeux@gmail.com>
2022-06-25 21:35:29 +0200

    Eight Version

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
-
+from bs4 import BeautifulSoup
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-		return "<h1>Bienvenue Anat</h1>"
+	data_set = {"Page" : "Home", "Message" : "Hello World"}
+    json_dump = json.dumps(data_set)
+    return json_dump

ea80ee0e2bd64c9a7c7594e9aedea804f27063fa
AnatoleT <anatole.titeux@gmail.com>
2022-06-25 21:30:31 +0200

    Sixth version

--- app/main.py
+++ app/main.py
@@ -4,20 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-	session = requests.Session()
-	my_headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", 
-          "Accept":"text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,image/apng,*/*;q=0.8"}
-	url = 'https://finviz.com/quote.ashx?t=TTE'
-	response = session.get(url, headers=my_headers)
-	soup = BeautifulSoup(response.text, 'html.parser')
-	tab = soup.find("table",{"class":"snapshot-table2"})
-	td = tab.find_all("td")
-	print(len(td))
-	for i in range (len(td)):
-		if td[i].text == "Dividend":
-        	nthDividend = td[i + 1]
-		dividendAmount = nthDividend.find("b")
-	return dividendAmount.text
-
-
-
+	return "Bienvenue Anat"

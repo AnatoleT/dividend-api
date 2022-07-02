7adb9b1c018a4acdc9d7ab17641a7a3d8e3bd24b
AnatoleT <anatole.titeux@gmail.com>
2022-07-02 11:27:09 +0200

    added image

--- app/main.py
+++ app/main.py
@@ -137,3 +137,27 @@ def all():
 	else:
 		json = '{ "Request":"Dividend", "Response": "Bad ticker"}'
 		return json
+
+#Get logo of the company
+@app.route("/stock/img")
+def price():
+	page = request.args.get('ticker', default = "goog", type = str)
+	session = requests.Session()
+	my_headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", 
+          "Accept":"text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,image/apng,*/*;q=0.8"}
+	url = 'https://finviz.com/quote.ashx?t=' + page
+	response = session.get(url, headers=my_headers)
+	if response.status_code == 200:
+		soup = BeautifulSoup(response.text, 'html.parser')
+		tab = soup.find("table",{"class":"fullview-title"})
+		td = tab.find("a", {"class": "tab-link"})
+		return td
+		for i in range (len(td)):
+			if td[i].text == "Price":
+				nthPrice = td[i + 1]
+				price = nthPrice.find("b").text
+				json = '{ "Request":"Image", "url":"' + price + '"}'
+				return json
+	else :
+		json = '{ "Request":"Dividend", "Response": "Bad ticker"}'
+		return json
